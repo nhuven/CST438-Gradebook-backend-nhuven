3b03418b30fcf6587ce5f51e546bccabc2592c81
nhuven <nathan.huven@gmail.com>
2022-09-22 21:49:34 -0700

    Added ProcessEnrollment in order to reuse code for updating the enrollment db between the Rest and MQ controllers

---
+++ cst438_gradebook/src/main/java/com/cst438/services/ProcessEnrollment.java
@@ -0,0 +1,46 @@
+package com.cst438.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cst438.domain.Course;
+import com.cst438.domain.Enrollment;
+import com.cst438.domain.EnrollmentDTO;
+import com.cst438.domain.EnrollmentRepository;
+
+
+
+@Service
+public class ProcessEnrollment {
+	
+	@Autowired
+	private EnrollmentRepository enrollmentRepository;
+	
+	public EnrollmentDTO processEnrollment(EnrollmentDTO enrollmentDTO, Course course) {
+
+		Enrollment enrollment = new Enrollment();
+		Enrollment newEnrollment = new Enrollment();
+		
+		
+		// check to see if the student is already enrolled in the course.
+		// if already enrolled will return the existing id instead of enrolling again
+		List<Enrollment> alreadyEnrolled = enrollmentRepository.findByStudentEmailAndCourse(enrollmentDTO.studentEmail, course);
+		
+		if(alreadyEnrolled.size() > 0) {
+			enrollment = alreadyEnrolled.get(0);
+		}
+		else {
+			newEnrollment.setStudentEmail(enrollmentDTO.studentEmail);
+			newEnrollment.setStudentName(enrollmentDTO.studentName);
+			newEnrollment.setCourse(course);
+			enrollment = enrollmentRepository.save(enrollment);
+		}
+		
+		enrollmentDTO.id = enrollment.getId();
+		
+		return enrollmentDTO;
+	}
+
+}
