2d719c53ad72a62dd588b1b27c0ae9891b22f318
nhuven <nathan.huven@gmail.com>
2022-09-22 20:53:52 -0700

    Updated addEnrollment to update db with enrollment details

--- cst438_gradebook/src/main/java/com/cst438/controllers/EnrollmentController.java
+++ cst438_gradebook/src/main/java/com/cst438/controllers/EnrollmentController.java
@@ -1,5 +1,7 @@
 package com.cst438.controllers;
 
+import java.sql.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.cst438.domain.Assignment;
 import com.cst438.domain.Course;
 import com.cst438.domain.CourseRepository;
 import com.cst438.domain.Enrollment;
@@ -31,10 +34,34 @@ public class EnrollmentController {
 	@Transactional
 	public EnrollmentDTO addEnrollment(@RequestBody EnrollmentDTO enrollmentDTO) {
 		
-		//TODO  complete this method in homework 4
 		
-		return null;
+		Course c = courseRepository.findById(enrollmentDTO.course_id).orElse(null);
 		
+		// Check that course exists
+		if (c == null) {
+			throw new ResponseStatusException( HttpStatus.BAD_REQUEST, "Course not found. " + enrollmentDTO.course_id );
+		}
+		
+		if (enrollmentDTO.studentEmail.isBlank()) {
+			throw new ResponseStatusException( HttpStatus.BAD_REQUEST, "Student email not provided." );
+		}
+		
+		if (enrollmentDTO.studentName.isBlank()) {
+			throw new ResponseStatusException( HttpStatus.BAD_REQUEST, "Student name not provided." );
+		}
+
+		Enrollment enrollment = new Enrollment();
+		
+		enrollment.setStudentEmail(enrollmentDTO.studentEmail);
+		enrollment.setStudentName(enrollmentDTO.studentName);
+		enrollment.setCourse(c);
+		
+		Enrollment newEnrollment = enrollmentRepository.save(enrollment);
+		
+		enrollmentDTO.id = newEnrollment.getId();
+		
+		return enrollmentDTO;
+
 	}
 
 }
