8461819897fca76cceab819aab6f1c59b7b40ccd
nhuven <nathan.huven@gmail.com>
2022-09-22 21:57:26 -0700

    updated recieve method to process enrollment request from MQ

--- cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceMQ.java
+++ cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceMQ.java
@@ -5,7 +5,9 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.cst438.domain.Course;
 import com.cst438.domain.CourseDTOG;
@@ -25,6 +27,9 @@ public class RegistrationServiceMQ extends RegistrationService {
 
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
+	
+	@Autowired
+	ProcessEnrollment pe;
 
 	public RegistrationServiceMQ() {
 		System.out.println("MQ registration service ");
@@ -43,9 +48,28 @@ public class RegistrationServiceMQ extends RegistrationService {
 	@RabbitListener(queues = "gradebook-queue")
 	@Transactional
 	public void receive(EnrollmentDTO enrollmentDTO) {
-		
 		//TODO  complete this method in homework 4
+		boolean validRequest = true;
+		Course c = courseRepository.findById(enrollmentDTO.course_id).orElse(null);
 		
+		// Check that course exists
+		if (c == null) {
+			System.out.println("RegistrationServiceREST: Enrollment request for invalid course.");
+			validRequest = false;
+		}
+		
+		else if (enrollmentDTO.studentEmail.isBlank()) {
+			System.out.println("RegistrationServiceREST: Student email not provided." );
+			validRequest = false;
+		}
+		
+		if (enrollmentDTO.studentName.isBlank()) {
+			System.out.println("RegistrationServiceREST: Student name not provided." );
+			validRequest = false;
+		}
+		
+		if(validRequest)
+			pe.processEnrollment(enrollmentDTO, c);
 	}
 
 	// sender of messages to Registration Service
